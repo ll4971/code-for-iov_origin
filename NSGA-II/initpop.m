e63cad3a95dc2a00f1b7862a93bf84541e264b5d
刘泊志 <1024645013@qq.com>
2023-12-13 17:27:36 +0800

    surface 12.13 17:27

--- NSGA-II/initpop.m
+++ NSGA-II/initpop.m
@@ -1,12 +1,12 @@
-function chrom = initpop(NP, M, dim, m, n, com, spc, COM, SPC, N, r ,Ur, D, x0 , rho, v,ka, epsilon, sigma, p)
+function [chrom, com, spc] = initpop(NP, M, dim, m, n, com, spc, COM, SPC, N, r ,Ur, D, x0 , rho, v,ka, epsilon, sigma, p)
 % K 是数组元素总个数将决策变量和目标函数值组合一个数组
 K = M + dim;
+counter = 0;% 初始化计数器
 %% 初始化每条染色体
 chrom = zeros(NP,K);
 for i = 1 : NP 
     % 初始化种群
     while 1
-        
         x = randi(n, 1, m); % 生成m个1~n的随机数，索引买家对应的卖家
         cs = zeros(1, n);  % 卖家计算资源实际供应量
         for k = 1 : n
@@ -14,6 +14,12 @@ for i = 1 : NP
         end
         if all(cs <= COM)
             break
+        else
+            counter = counter + 1; % 不满足条件时增加计数
+            if counter >= 100
+                com = max(floor(com - 1./Ur), 0); % 当计数器达到10时，com内的值全部减10/Ur
+                counter = 0; % 重置计数器
+            end
         end
     end
     while 1
@@ -24,11 +30,20 @@ for i = 1 : NP
         end
         if all(ss <= SPC)
             break
+        else
+            counter = counter + 1; % 不满足条件时增加计数
+            if counter >= 100
+                spc = max(floor(spc - 1./Ur), 0); % 当计数器达到10时，com内的值全部减10/Ur
+                counter = 0; % 重置计数器
+            end
         end
     end
     chrom(i,1:dim) = [x y];
 end
+
 for i = 1 : NP
     chrom(i,dim + 1: K) = fitness(chrom(i,:), m, n, com, spc, COM, SPC, N, r ,Ur, D, x0 , rho, v, ka, epsilon, sigma, p);
 end
 end
+
+
--- NSGA-II/main.m
+++ NSGA-II/main.m
@@ -3,33 +3,35 @@ clc
 clear
 close all
 num_experiments = 1;
-
-%% 输入数据
-% 输入买家和卖家的数量
-m = 50; % 买家数量
-n = 7; % 卖家数量
-swt = 1; % 1：添加紧急程度；0：取消紧急程度
-rep = 0; % 1:存在信誉变化；0：不存在信誉变化
-% 调用 generate_data 函数生成需求和供给数据
-[com, spc, COM, SPC, Ur, r, N, D, x0] = generate_data(m, n, swt, rep);
+%% 算法数据
+NP = 100;          % 种群数量
+maxgen = 200;     % 迭代次数
+Pc = 0.8;
+Pm = 0.2;
+M = 2;            % 目标函数个数
 rho = 0.5; % 最大收益目标函数买家花费占比
-ka = 10;   %
+ka = 10;  
 v = 0.5;    % 最大收益目标函数卖家收入占比
 epsilon = 1;
 sigma = 6;
 p = 1000; % 传输功率
-%% 算法数据
-NP = 80;          % 种群数量
-maxgen = 6000;     % 迭代次数
-Pc = 0.8;
-Pm = 0.2;
-M = 2;            % 目标函数个数
-dim = m * 2 ;      % 决策变量维数
+T_delay = zeros(num_experiments,1); % 总延迟时间
+delay_average_results = zeros(num_experiments,1); % 平均延迟时间
+x = zeros(num_experiments,1); % 车辆数横坐标
 %% 主循环
 for times = 1:num_experiments
-    rng(8);
+    rng(times);
+    %% 输入数据
+    % 输入买家和卖家的数量
+    m = 80*times; % 买家数量
+    n = 3; % 卖家数量
+    swt = 1; % 1：添加紧急程度；0：取消紧急程度
+    rep = 0; % 1:存在信誉变化；0：不存在信誉变化
+    % 调用 generate_data 函数生成需求和供给数据
+    [com, spc, COM, SPC, Ur, r, N, D, x0] = generate_data(m, n, swt, rep);
+    dim = m * 2 ;      % 决策变量维数
     %% 初始化种群
-    chrom = initpop(NP, M, dim, m, n, com, spc, COM, SPC, N, r ,Ur, D, x0 ,rho, v,ka, epsilon, sigma, p);
+    [chrom, com, spc] = initpop(NP, M, dim, m, n, com, spc, COM, SPC, N, r ,Ur, D, x0 ,rho, v,ka, epsilon, sigma, p);
     
     %% 非支配排序（non-domination-sort）
     chrom = nonDominatedSort(chrom, M, dim );
@@ -53,7 +55,7 @@ for times = 1:num_experiments
         % 子代个体和父代个体融合
         Nc = size(chrom,1);
         Nn = size(newchrom,1);
-        allchrom(1:Nc,:) = chrom;
+        allchrom = chrom;
         allchrom(Nc + 1 : Nc + Nn,1 : M+dim) = newchrom;
       
         % 融合种群非支配排序
@@ -72,8 +74,18 @@ for times = 1:num_experiments
         delay(gen) = toc; % 记录迭代结束时间，并计算出延迟
         %% 计算并打印累积用时
         elapsed_time(gen) = toc(start_time);
-        fprintf('累积用时：%.2f秒\n', toc(start_time)); 
+        fprintf('累积用时：%.2f秒\n', toc(start_time));
     end
+
+    %% 传输延迟 + 计算延迟
+    y = chrom(1, m + 1: dim);
+    for i = 1:m
+        h = 1e-8 + exp(2-5*log10(D(i,y(i))));
+        T_delay(times) = T_delay(times) + sum(x0 ./ com) + x0(i) / (spc(i) * log2(1 + p * h /sigma^2));
+    end
+    %% 平均延迟
+    delay_average_results(times) = elapsed_time(maxgen)/maxgen;
+
     %% 结果输出
     clc
     gbest = chrom(:,1:dim+M);
@@ -100,18 +112,23 @@ for times = 1:num_experiments
     plot(elapsed_time) 
     xlabel('迭代次数') 
     ylabel('累积用时') 
-    title('累积用时随迭代次数的变化') 
-    
-    %%保存至excel
+    title('累积用时随迭代次数的变化')
+    x(times) = m; 
+    figure(5) 
+    plot(x, T_delay)
+    xlabel('车辆数') 
+    ylabel('总延迟') 
+    title('总延迟随车辆数的变化')
+    %% 保存至excel
     if swt == 0
-        file_path = '../NAGA-II_results'; % 修改为你希望保存的文件夹路径
+        file_path = '../NSGA-II_results'; % 修改为你希望保存的文件夹路径
     elseif swt == 1
-        file_path = '../NAGA-II_results_Ur'; % 修改为你希望保存的文件夹路径
+        file_path = '../NSGA-II_results_Ur'; % 修改为你希望保存的文件夹路径
     end
     %保存帕累托解集
     file_name_01 = 'Pareto_results.xlsx'; % 修改为你希望保存的文件名
-    gbest(:, 21) = -gbest(:, 21);   % 21列取反
-    file_restore_01 = gbest(:,[21:22]);   %保存21和22列
+    gbest(:, dim+1) = -gbest(:, dim+1);   % 21列取反
+    file_restore_01 = gbest(:,[dim+1:dim+2]);   %保存21和22列
     
     %保存总价值
     file_name_02 = 'Revenue_results.xlsx'; % 修改为你希望保存的文件名
@@ -120,10 +137,27 @@ for times = 1:num_experiments
     %保存总能耗
     file_name_03 = 'Consumption_results.xlsx'; % 修改为你希望保存的文件名
     file_restore_03 = FG2;   %保存21和22列
+
+    %保存总延迟
+    file_name_04 = 'T_delay_results.xlsx'; % 修改为你希望保存的文件名
+    file_restore_04 = [x, T_delay];
+
+    %保存算法平均延迟
+    file_name_05 = 'delay_average_results.xlsx'; % 修改为你希望保存的文件名
+    file_restore_05 = delay_average_results;
     
+    %保存算法总延迟
+    file_name_06 = 'elapsed_time_results.xlsx'; % 修改为你希望保存的文件名
+    file_restore_06 = elapsed_time;
+
+
+
     % 使用 xlswrite 函数保存数据到 Excel 文件中
-%     xlswrite(fullfile(file_path, file_name_01), file_restore_01, times, 'A1'); % 将数据从 A1 单元格开始保存
-%     xlswrite(fullfile(file_path, file_name_02), file_restore_02, times, 'A1'); % 将数据从 A1 单元格开始保存
-%     xlswrite(fullfile(file_path, file_name_03), file_restore_03, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_01), file_restore_01, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_02), file_restore_02, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_03), file_restore_03, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_04), file_restore_04, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_05), file_restore_05, times, 'A1'); % 将数据从 A1 单元格开始保存
+    xlswrite(fullfile(file_path, file_name_06), file_restore_06, times, 'A1'); % 将数据从 A1 单元格开始保存
 end
 
